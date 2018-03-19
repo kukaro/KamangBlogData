7edb17be104449e74f4e0dd864f35bd856b14102
KTJ <justkukaro@naver.com>
2018-03-19 16:13:08 +0900

    블로그 파일

---
+++ JUnit/Ch02/Test/JUnitUse/src/test/net/theceres/PersonTest.java
@@ -0,0 +1,20 @@
+package net.theceres;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+    @Test
+    @DisplayName("Person's nameDecorate method's test")
+    void nameDecorate() {
+        Person p = new Person("kukaro", 26);
+        assertEquals("kukarogood", p.nameDecorate("good"),"success");
+    }
+
+    @Test
+    void isMatchAge() {
+    }
+
+}
---
+++ JUnit/log.md
@@ -0,0 +1,7 @@
+#JUnit
+##Ch02
+###Test
+####JUnitUse
+Person의 Test를 생성  
+http://ant.apache.org/manual/tutorial-HelloWorldWithAnt.html  
+위의 링크에서 ant에서 junit을 활용하는 방법에 대해서 나와있다.  
