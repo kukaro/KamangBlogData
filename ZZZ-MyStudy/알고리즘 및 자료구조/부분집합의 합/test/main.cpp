40fed24c802f937c6712ae636c16c23fe2495762
kukaro <justkukaro@naver.com>
2018-03-25 08:22:30 +0000

    코드작성

--- ZZZ-MyStudy/알고리즘 및 자료구조/부분집합의 합/test/main.cpp
+++ ZZZ-MyStudy/알고리즘 및 자료구조/부분집합의 합/test/main.cpp
@@ -2,27 +2,30 @@
 #include <vector>
 using namespace std;
 
-int N, M;
-vector<vector<int>> G;
+int M,N;
+vector<int> arr;
 
-int f(int n, int m) {
-	if(G[n][m]==0){
-		return 0;
+bool f(int n,int m){
+	if(n==1){
+		if(m==0){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
-  if(n==1 && m==1){
-		return 1;
-	}
-	return (n - 1 > 0 ? f(n - 1, m) : 0) + (m - 1 > 0 ? f(n, m - 1) : 0);
+	return f(n-1,m-arr[n])-f(n-1,m);
 }
 
 int main() {
-  cin >> N >> M;
-  G.resize(N + 1, vector<int>(M + 1, 0));
-  for (int i = 1; i <= N; i++) {
-    for (int j = 1; j <= M; j++) {
-      cin >> G[i][j];
-    }
-  }
-  cout << f(N, M);
-  return 0;
+	int tmp;
+  cout<<"input m, n: ";
+	cin>>M>>N;
+	arr.push_back(0);
+	for(int i=0;i<N;i++){
+		cin>>tmp;
+		arr.push_back(tmp);
+	}
+	cout<<f(N,M);
+	return 0;
 }
