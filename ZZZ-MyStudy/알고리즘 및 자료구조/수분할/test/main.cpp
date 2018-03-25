f13e0eaf2d80e037638ee44084f04b10fcdc81d7
Ubuntu <ubuntu@ip-172-31-3-164.ap-northeast-2.compute.internal>
2018-03-25 06:44:52 +0000

    수분할

--- ZZZ-MyStudy/알고리즘 및 자료구조/수분할/test/main.cpp
+++ ZZZ-MyStudy/알고리즘 및 자료구조/수분할/test/main.cpp
@@ -3,43 +3,24 @@
 #define MAXSIZE 1000
 using namespace std;
 
-int cacheCount;
-vector<int> arr;
-vector<vector<int>> memo;
+int N,M;
 
-int f(int m, int n) {
-  int result = 0;
-  int bigCache = arr[n - 1];
-  int iter = m / bigCache;
-  if (n == 1) {
-    if (m % bigCache == 0) {
-      return 1;
-    } else {
-      return 0;
-    }
-  }
-	if(memo[m][n]>0){
-		return memo[m][n];
+int f(int n,int m){
+	int result=0;
+	if(n-m<0){
+		return 0;
+	}else if(n-m==0){
+		return 1;
 	}
-  for (int i = 0; i <= iter; i++) {
-    result += memo[m - bigCache * i][ n - 1] = f(m - bigCache * i, n - 1);
-  }
-  return result;
-}
+	for(int i=1;i<=m;i++){
+		result += f(n-m,i);
+	}
+	return result;
+};
 
-int main() {
-  int tmp;
-  int money;
-  cout << "input number of bills : ";
-  cin >> cacheCount;
-  cout << "input biils : ";
-  for (int i = 0; i < cacheCount; i++) {
-    cin >> tmp;
-    arr.push_back(tmp);
-  }
-  cout << "input money : ";
-  cin >> money;
-  memo.resize(money + 1, vector<int>(money+1,0));
-  cout << f(money, cacheCount)<<endl;
-  return 0;
+int main(){
+	cout<<"input N, M : ";
+	cin>>N>>M;
+	cout<<"total : "<<f(N+M,M)<<endl;;
+	return 0;
 }
