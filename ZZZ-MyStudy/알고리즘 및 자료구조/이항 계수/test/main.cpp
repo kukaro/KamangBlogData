f53eeeabd610b064e54df3e56d80781c395adc20
Ubuntu <ubuntu@ip-172-31-3-164.ap-northeast-2.compute.internal>
2018-03-25 03:19:05 +0000

    메모이제이션 설명

--- ZZZ-MyStudy/알고리즘 및 자료구조/이항 계수/test/main.cpp
+++ ZZZ-MyStudy/알고리즘 및 자료구조/이항 계수/test/main.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include <vector>
 #define ll long long
-#define MAXSIZE 10000
+#define MAXSIZE 1000
 
 using namespace std;
 
-vector<vector<ll>> memo;
+vector<vector<ll>> memo(MAXSIZE, vector<ll>(MAXSIZE, 0));
 int N, R;
 
 ll c(int n, int r) {
-
+  cout << n << ":" << r << endl;
+  if (memo[n][r] > 0) {
+    return memo[n][r];
+  }
   if (r == 0 || n == r) {
     return memo[n][r] = 1;
   }
-  return c(n - 1, r - 1) + c(n - 1, r);
+  return memo[n][r] = c(n - 1, r - 1) + c(n - 1, r);
 }
 
 int main() {
-  cout << " input n, r : ";
+  cout << "input n, r : ";
   cin >> N >> R;
   cout << c(N, R);
   return 0;
