27f84f066df67c3db6dc479c048ca0df28996d76
Ubuntu <ubuntu@ip-172-31-3-164.ap-northeast-2.compute.internal>
2018-03-25 03:36:29 +0000

    금액 맞추기 푸는 중 임시저장

--- ZZZ-MyStudy/알고리즘 및 자료구조/금액 맞추기/test/main.cpp
+++ ZZZ-MyStudy/알고리즘 및 자료구조/금액 맞추기/test/main.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
 #include <vector>
-#define ll long long
 #define MAXSIZE 1000
-
 using namespace std;
 
-vector<vector<ll>> memo(MAXSIZE, vector<ll>(MAXSIZE, 0));
-int N, R;
+int cacheCount;
+vector<int> arr;
 
-ll c(int n, int r) {
-  cout << n << ":" << r << endl;
-  if (memo[n][r] > 0) {
-    return memo[n][r];
-  }
-  if (r == 0 || n == r) {
-    return memo[n][r] = 1;
-  }
-  return memo[n][r] = c(n - 1, r - 1) + c(n - 1, r);
+int f(int n){
 }
 
-int main() {
-  cout << "input n, r : ";
-  cin >> N >> R;
-  cout << c(N, R);
-  return 0;
+int main(){
+	int tmp
+	cout<<"input number of bills : ";
+	cin>>cacheCount;
+	cout<<"input biils : ";
+	for(int i=0;i<cacheCount;i++){
+	}
+	cin>>tmp;
+	cout<<"input money : ";
+	return 0;
 }
