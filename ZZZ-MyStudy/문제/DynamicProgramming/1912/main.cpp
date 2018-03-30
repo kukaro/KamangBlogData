e577c9ead15d8e0512283c9c8800176bb2d7d076
kukaro <justkukaro@naver.com>
2018-03-30 10:20:58 +0000

    메모이제이션

--- ZZZ-MyStudy/문제/DynamicProgramming/1912/main.cpp
+++ ZZZ-MyStudy/문제/DynamicProgramming/1912/main.cpp
@@ -1,33 +1,40 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#define MAXSIZE 100005
 using namespace std;
 
 int N;
 vector<int> arr;
 int maxVal;
+vector<int> memo(MAXSIZE, 0);
 
-int f(int n){
-	if(n>N){
-		return 0;
-	}
-	if(f(n+1)>0){
-		maxVal = maxVal>f(n+1)+arr[n]?maxVal:f(n+1)+arr[n];
-		return f(n+1)+arr[n];
-	}else{
-		maxVal = maxVal>arr[n]?maxVal:arr[n];
-		return arr[n];
-	}
+int f(int n) {
+  int tmp = 0;
+  if (n > N) {
+    return 0;
+  }
+  if (memo[n + 1] == 0) {
+    memo[n + 1] = f(n + 1);
+  }
+  tmp = memo[n + 1];
+  if (tmp > 0) {
+    maxVal = maxVal > tmp + arr[n] ? maxVal : tmp + arr[n];
+    return tmp + arr[n];
+  } else {
+    maxVal = maxVal > arr[n] ? maxVal : arr[n];
+    return arr[n];
+  }
 }
 
-int main(){
-	int tmp;
-	cin>>N;
-	arr.push_back(0);
-	for(int i=1;i<=N;i++){
-		cin>>tmp;
-		arr.push_back(tmp);
-	}
-	f(1);
-	cout<<maxVal<<endl;
-	return 0;
+int main() {
+  int tmp;
+  cin >> N;
+  arr.push_back(0);
+  for (int i = 1; i <= N; i++) {
+    cin >> tmp;
+    arr.push_back(tmp);
+  }
+  f(1);
+  cout << maxVal << endl;
+  return 0;
 }
