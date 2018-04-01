7a1fa3c780a2943d3818d0c5dcdda36c7997a2f9
kukaro <justkukaro@naver.com>
2018-04-01 07:26:25 +0000

    5585 1차

--- ZZZ-MyStudy/문제/GreedyAlgorithm/5585/main.cpp
+++ ZZZ-MyStudy/문제/GreedyAlgorithm/5585/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
+int M;
+vector<int> arr;
 
+void initVec() {
+  arr.push_back(500);
+  arr.push_back(100);
+  arr.push_back(50);
+  arr.push_back(10);
+  arr.push_back(5);
+  arr.push_back(1);
+}
+
+int main() {
+  int count=0;
+	cin >> M;
+  M = 1000 - M;
+  initVec();
+	for(int i=0;i<arr.size();i++){
+		if(M>arr[i]){
+			count+=M/arr[i];
+			M-=M/arr[i]*arr[i];
+		}
+	}
+	cout<<count<<endl;
   return 0;
 }
