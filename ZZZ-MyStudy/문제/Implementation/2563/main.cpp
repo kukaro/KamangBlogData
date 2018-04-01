fd1db49652cd3726c16a3ab902b4a3e85cc6b3c5
kukaro <justkukaro@naver.com>
2018-04-01 07:14:09 +0000

    2563

--- ZZZ-MyStudy/문제/Implementation/2563/main.cpp
+++ ZZZ-MyStudy/문제/Implementation/2563/main.cpp
@@ -1,57 +1,27 @@
 #include <iostream>
 #include <vector>
+#define MAXSIZE 101
 using namespace std;
 
-class Point {
-public:
-  int x;
-  int y;
-  Point(int x, int y) : x(x), y(y) {}
-  string info() { return to_string(x) + ":" + to_string(y); }
-};
-
 int N;
-vector<Point> arr;
 int area;
-
-void pointSortX(Point &a, Point &b) {
-  Point tmp = a;
-  if (a.x > b.x) {
-    a.x = b.x;
-    b.x = tmp.x;
-  }
-}
-
-void pointSortY(Point &a, Point &b) {
-  Point tmp = a;
-  if (a.y > b.y) {
-    a.y = b.y;
-    b.y = tmp.y;
-  }
-}
-
-int duplicateArea(Point a, Point b) {
-  int tx, ty;
-  pointSortX(a, b);
-  tx = a.x + 10 - b.x;
-  pointSortY(a, b);
-  ty = a.y + 10 - b.y;
-  return tx * ty > 0 ? tx * ty : 0;
-}
+vector<vector<int>> arr(MAXSIZE,vector<int>(MAXSIZE,0));
 
 int main() {
   int tx, ty;
   cin >> N;
+	int count = 0;
   for (int i = 0; i < N; i++) {
     cin >> tx >> ty;
-    arr.push_back(Point(tx, ty));
-  }
-  area = N * 100;
-  for (int i = 0; i < N; i++) {
-    for (int j = i + 1; j < N; j++) {
-      area -= duplicateArea(arr[i], arr[j]);
-    }
-  }
-  cout << area << endl;
+  	for(int i=tx+1;i<=tx+10;i++){
+			for(int j=ty+1;j<=ty+10;j++){
+				if(arr[i][j]!=1){
+					arr[i][j]=1;
+					count++;
+				}
+			}
+		}
+	}
+	cout<<count<<endl;
   return 0;
 }
