01a20120969e50c19fa0ad3f6017866fba03d904
kukaro <justkukaro@naver.com>
2018-04-03 01:59:15 +0900

    btn

--- Ant/Ch10/Test/test.c
+++ Ant/Ch10/Test/test.c
@@ -1,18 +1,3 @@
-const int pinLED = 3;
-const int pinREG = A5;
-
-long val;
-
-void setup() {
-  pinMode(pinREG,INPUT);
-  pinMode(pinLED,OUTPUT);
-}
-
-void loop() {
-  val = analogRead(pinREG);
-  val = val*255/1023;
-  analogWrite(pinLED,val);
-}
 
 const int pinBTN = 4;
 
