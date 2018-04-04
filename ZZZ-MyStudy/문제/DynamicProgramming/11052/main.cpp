6d7a252204a37c31aa0aca79196579233c97e396
kukaro <justkukaro@naver.com>
2018-04-04 00:49:24 +0000

    11052 1차 제출

--- ZZZ-MyStudy/문제/DynamicProgramming/11052/main.cpp
+++ ZZZ-MyStudy/문제/DynamicProgramming/11052/main.cpp
@@ -2,7 +2,32 @@
 #include <vector>
 using namespace std;
 
-int main(){
-	
-	return 0;
+int N;
+vector<int> arr;
+
+int f(int n) {
+  int max = 0;
+	if (n == 0) {
+    return 0;
+  }
+  for (int i = 1; i <= n; i++) {
+    if (n - i >= 0) {
+      if (max < f(n - i) + arr[i]) {
+        max = f(n - i) + arr[i];
+      }
+    }
+  }
+  return max;
+}
+
+int main() {
+  int tmp;
+  cin >> N;
+  arr.push_back(0);
+  for (int i = 1; i <= N; i++) {
+    cin >> tmp;
+    arr.push_back(tmp);
+  }
+  cout << f(N) << endl;
+  return 0;
 }
