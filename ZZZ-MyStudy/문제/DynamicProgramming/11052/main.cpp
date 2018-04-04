890424d16471d16d4232d6d9793c34181909af5b
kukaro <justkukaro@naver.com>
2018-04-04 00:54:09 +0000

    11052 2차 제출

--- ZZZ-MyStudy/문제/DynamicProgramming/11052/main.cpp
+++ ZZZ-MyStudy/문제/DynamicProgramming/11052/main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <vector>
+#define MAXSIZE 10005
 using namespace std;
 
 int N;
 vector<int> arr;
+vector<vector<int>> memo(MAXSIZE, vector<int>(MAXSIZE, 0));
 
 int f(int n) {
   int max = 0;
-	if (n == 0) {
+  if (n == 0) {
     return 0;
   }
   for (int i = 1; i <= n; i++) {
     if (n - i >= 0) {
-      if (max < f(n - i) + arr[i]) {
-        max = f(n - i) + arr[i];
+      if (memo[n - i] == 0) {
+        memo[n - i] = f(n - i);
+      }
+      if (max < memo[n - i] + arr[i]) {
+        max = memo[n - i] + arr[i];
       }
     }
   }
