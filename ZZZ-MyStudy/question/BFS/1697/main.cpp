d8a2b3f4e5f6f33dacc639f3267f5020f38ca6d3
kukaro <justkukaro@naver.com>
2018-04-06 02:36:23 +0900

    1697

--- ZZZ-MyStudy/question/BFS/1697/main.cpp
+++ ZZZ-MyStudy/question/BFS/1697/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-#define MAXSIZE 100005
+#define MAXSIZE 200000
 using namespace std;
 
 int N, K;
@@ -9,33 +9,33 @@ vector<int> arr(MAXSIZE, -1);
 queue<int> q;
 
 void bfs() {
-  int tmp;
-  arr[N] = 0;
- 	q.push(N);
-  while (!q.empty()) {
-    tmp = q.front();
-    q.pop();
-		if (tmp == K) {
-      break;
+    int tmp;
+    arr[N] = 0;
+    q.push(N);
+    while (!q.empty()) {
+        tmp = q.front();
+        q.pop();
+        if (tmp == K) {
+            break;
+        }
+        if (tmp - 1 >= 0 && arr[tmp - 1] == -1) {
+            q.push(tmp - 1);
+            arr[tmp - 1] = arr[tmp] + 1;
+        }
+        if (tmp + 1 <= MAXSIZE && arr[tmp + 1] == -1) {
+            q.push(tmp + 1);
+            arr[tmp + 1] = arr[tmp] + 1;
+        }
+        if (2 * tmp < MAXSIZE && arr[2 * tmp] == -1) {
+            q.push(2 * tmp);
+            arr[2 * tmp] = arr[tmp] + 1;
+        }
     }
-		if(tmp-1>0 && arr[tmp-1]==-1){
-    	q.push(tmp - 1);
-			arr[tmp-1]=arr[tmp]+1;
-		}
-		if(tmp+1<=MAXSIZE && arr[tmp+1]==-1){
-    	q.push(tmp + 1);
-			arr[tmp+1]=arr[tmp]+1;
-		}
-		if(2*tmp>N && arr[2*tmp]==-1){
-    	q.push(2 * tmp);
-			arr[2*tmp]=arr[tmp]+1;
-		}
-  }
 }
 
 int main() {
-  cin >> N >> K;
-  bfs();
-  cout << arr[K] << endl;
-  return 0;
+    cin >> N >> K;
+    bfs();
+    cout << arr[K] << endl;
+    return 0;
 }
