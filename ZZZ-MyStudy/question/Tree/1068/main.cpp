d88af2e5995abcf5537eec4a71d4b44e5181a0b6
kukaro <justkukaro@naver.com>
2018-04-13 14:28:41 +0900

    teamviewer

--- ZZZ-MyStudy/question/Tree/1068/main.cpp
+++ ZZZ-MyStudy/question/Tree/1068/main.cpp
@@ -1,28 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
+#define MAXSIZE 1005
 using namespace std;
 
 int cnt;
 int N, K;
 
-vector<int> child;
-vector<int> tree;
+int child[MAXSIZE];
+int parent[MAXSIZE];
 
 int main() {
     int tmp;
     cin >> N;
-    child.resize(N,0);
     for (int i = 0; i < N; i++) {
         cin >> tmp;
-        tree.push_back(tmp);
-        child[tmp]=1;
+        parent[i]=tmp;
+        child[tmp]++;
     }
-    cin>>K;
-    for (int atom:child) {
-        cout << atom << " ";
+    cin >> K;
+    child[K] = -1;
+    child[parent[K]]--;
+    for (int i = 0; i < N; i++) {
+        cout<<child[i]<<" ";
     }
-    cout << endl;
+    cout<<endl;
+    for (int i = 0; i < N; i++) {
+        if(child[parent[i]]==-1){
+            child[i]=-1;
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        cout<<child[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<N;i++){
+        if(child[i]==0){
+            cnt++;
+        }
+    }
+    cout<<cnt<<endl;
     return 0;
 }
