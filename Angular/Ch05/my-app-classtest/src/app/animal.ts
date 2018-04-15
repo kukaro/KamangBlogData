6076fa89c80f11fe13d131eb84a97acede1689b0
kukaro <justkukaro@naver.com>
2018-04-16 08:19:17 +0900

    range for

---
+++ Angular/Ch05/my-app-classtest/src/app/animal.ts
@@ -0,0 +1,26 @@
+export class Animal {
+  private _name: string;
+  private _foot: number;
+
+  constructor(name: string, foot: number) {
+    this._name = name;
+    this._foot = foot;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get foot(): number {
+    return this._foot;
+  }
+
+  set foot(value: number) {
+    this._foot = value;
+  }
+}
+
---
+++ Angular/Ch05/my-app-classtest/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {Animal} from './animal';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  animalList = [new Animal('elephant', 4),
+    new Animal('kukaro', 2,
+    )];
+  objectKeys = Object.keys;
+}
