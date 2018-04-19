3ddb04f04d921723b9afbc04fecc3b7774459e4d
kukaro <justkukaro@naver.com>
2018-04-19 20:39:16 +0900

    style bind

---
+++ Angular/Ch07/templatetest04/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  myColor = 'red';
+  myFontSize = 15;
+  myBackColor = 'green';
+}
