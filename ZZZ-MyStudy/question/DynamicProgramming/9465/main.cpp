f659db83eacfe6d1dbfa0a865fa7152a9204d8f6
kukaro <justkukaro@naver.com>
2018-04-24 22:39:17 +0000

    9465 1차 제출

--- ZZZ-MyStudy/question/DynamicProgramming/9465/main.cpp
+++ ZZZ-MyStudy/question/DynamicProgramming/9465/main.cpp
@@ -1,52 +1,64 @@
 #include <iostream>
 #include <vector>
-#define MAXSIZE 105
+#define MAXSIZE 100005
 using namespace std;
 
 int T, N;
 vector<int> arr;
 vector<int> result;
+vector<vector<vector<int>>> memo(MAXSIZE,
+                                 vector<vector<int>>(2, vector<int>(2, 0)));
 
-int f(int n, bool sw) {
-  int sum = 0;
-  int a = 0, b = 0;
-	cout<<arr[n]<<":"<<sw<<endl;
-  if (n > N || n <= 0) {
+int f(int n, bool isChoice, bool isTop) {
+  int add = 0;
+  int next = 0;
+  int cnt = 0;
+  if (n > N) {
     return 0;
   }
-  if (n > N) {
-    a = f(n - N + 1, false) > f(n - N + 1, true) ? f(n - N + 1, false)
-                                                 : f(n - N + 1, true);
-    b = f(n - N - 1, false) > f(n - N - 1, true) ? f(n - N - 1, false)
-                                                 : f(n - N - 1, true);
-    sum = a + b;
+  if (isTop) {
+    cnt = n;
   } else {
-    a = f(n + N + 1, false) > f(n + N + 1, true) ? f(n + N + 1, false)
-                                                 : f(n + N + 1, true);
-    b = f(n + N - 1, false) > f(n + N - 1, true) ? f(n + N - 1, false)
-                                                 : f(n + N - 1, true);
-    sum = a + b;
+    cnt = n + N;
   }
-  return sum + sw ? arr[n] : 0;
+  if (memo[n + 1][true][!isTop] == 0) {
+    memo[n + 1][true][!isTop] = f(n + 1, true, !isTop);
+  }
+  if (memo[n + 1][false][!isTop] == 0) {
+    memo[n + 1][false][!isTop] = f(n + 1, false, !isTop);
+  }
+  next = memo[n + 1][true][!isTop] > memo[n + 1][false][!isTop]
+             ? memo[n + 1][true][!isTop]
+             : memo[n + 1][false][!isTop];
+  if (isChoice) {
+    add = arr[cnt];
+  } else {
+    if (memo[n + 1][true][isTop] == 0) {
+      memo[n + 1][true][isTop] = f(n + 1, true, isTop);
+    }
+    if (memo[n + 1][false][isTop] == 0) {
+      memo[n + 1][false][isTop] = f(n + 1, false, isTop);
+    }
+    next = next > memo[n + 1][true][isTop] ? next : memo[n + 1][true][isTop];
+    next = next > memo[n + 1][false][isTop] ? next : memo[n + 1][false][isTop];
+  }
+  return next + add;
 }
 
 int main() {
   int tmp;
-  int max = 0;
   cin >> T;
   for (int t = 0; t < T; t++) {
     cin >> N;
     arr.clear();
     arr.push_back(0);
+    memo.clear();
+    memo.resize(MAXSIZE, vector<vector<int>>(2, vector<int>(2, 0)));
     for (int n = 1; n <= 2 * N; n++) {
       cin >> tmp;
       arr.push_back(tmp);
     }
-    for (int n = 1; n <= 2 * N; n++) {
-      tmp = f(n, true);
-      max = max > tmp ? max : tmp;
-    }
-    result.push_back(max);
+    result.push_back(f(0, false, false));
   }
   for (int t = 0; t < T; t++) {
     cout << result[t] << endl;
