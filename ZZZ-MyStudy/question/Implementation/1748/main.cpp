9592922bdf607978e84281c68441812a93dc5dd0
kukaro <justkukaro@naver.com>
2018-04-25 14:47:05 +0000

    1748

---
+++ ZZZ-MyStudy/question/Implementation/1748/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int sum = 0;
+
+int main() {
+  int tn;
+  int carry = 10;
+  int cnt = 1;
+  cin >> N;
+  tn = N;
+  while (true) {
+    if (tn > carry) {
+      sum += (carry - carry / 10) * cnt;
+      cnt++;
+      carry *= 10;
+    } else {
+      sum += (tn % (carry / 10) + 1) * cnt;
+      break;
+    }
+  }
+  cout << sum << endl;
+  return 0;
+}
