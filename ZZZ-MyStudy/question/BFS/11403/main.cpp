2e509b45739411956187d0d273f60876a8b0d332
kukaro <justkukaro@naver.com>
2018-04-26 22:10:50 +0000

    11403 1ck

--- ZZZ-MyStudy/question/BFS/11403/main.cpp
+++ ZZZ-MyStudy/question/BFS/11403/main.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
-#include <queue>
 #include <vector>
 using namespace std;
-
-class Point {
-public:
-  int x;
-  int y;
-
-	Point(){}
-  Point(int x, int y) : x(x), y(y) {}
-};
 
 int N;
 vector<vector<int>> G;
 vector<vector<int>> ans;
-queue<Point> q;
+
+void f(int s,int x){
+	for(int i=1;i<=N;i++){
+		if(G[x][i]==1 && ans[s][i]==0){
+			ans[s][i]=1;
+			f(s,i);
+		}
+	}
+}
 
 int main() {
   int tmp;
 	int cur;
-	Point p;
 
   cin >> N;
   G.resize(N + 1, vector<int>(N + 1, 0));
@@ -30,23 +27,17 @@ int main() {
     for (int j = 1; j <= N; j++) {
       cin >> tmp;
       G[i][j] = tmp;
-      if (tmp == 1) {
-        q.push(Point(i, j));
-      }
     }
   }
 	
-	while(!q.empty()){
-		p = q.front();
-		q.pop();
-		cur = p.x;
-		
+	for (int i=1;i<=N;i++){
+		f(i,i);
 	}
-  //	for(int i=1;i<=N;i++){
-  //		for(int j=1;j<=N;j++){
-  //			cout<<G[i][j]<<" ";
-  //		}
-  //		cout<<endl;
-  //	}
+	for(int i=1;i<=N;i++){
+		for(int j=1;j<=N;j++){
+			cout<<ans[i][j]<<" ";
+		}
+		cout<<endl;
+	}
   return 0;
 }
