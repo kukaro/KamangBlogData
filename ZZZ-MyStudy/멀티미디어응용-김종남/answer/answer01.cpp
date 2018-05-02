1f884fa396f3ec355e2a372288adc78e81059d86
kukaro <justkukaro@naver.com>
2018-05-02 10:31:46 +0900

    chang

--- ZZZ-MyStudy/멀티미디어응용-김종남/answer/answer01.cpp
+++ ZZZ-MyStudy/멀티미디어응용-김종남/answer/answer01.cpp
@@ -7,9 +7,10 @@ using namespace cv;
 int main(int argc, const char *argv[]) {
     Mat image(400,600,CV_8UC3,Scalar(255,255,255));
     Point pt(100,100);
-    circle(image,pt,100,Scalar(255,0,0),1);
-    ellipse(image,Point(50,100),Size(50,50),0,0,180,Scalar(255,0,0));
-    ellipse(image,Point(150,100),Size(50,50),0,0,-180,Scalar(255,0,0));
+    ellipse(image,Point(100,100),Size(100,100),0,0,-180,Scalar(0,0,255),FILLED);
+    ellipse(image,Point(100,100),Size(100,100),0,0,180,Scalar(255,0,0),FILLED);
+    ellipse(image,Point(50,100),Size(50,50),0,0,180,Scalar(0,0,255),FILLED);
+    ellipse(image,Point(150,100),Size(50,50),0,0,-180,Scalar(255,0,0),FILLED);
     imshow("result",image);
     waitKey();
     return 0;
