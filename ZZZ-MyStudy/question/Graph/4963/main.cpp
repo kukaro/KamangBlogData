21ed8d5ba144c63243d78dd6f51e359f08983258
kukaro <justkukaro@naver.com>
2018-05-09 02:08:50 +0000

    4963 fix

--- ZZZ-MyStudy/question/Graph/4963/main.cpp
+++ ZZZ-MyStudy/question/Graph/4963/main.cpp
@@ -6,32 +6,32 @@ vector<vector<int>> G;
 vector<int> result;
 int W, H;
 
-void f(int h,int w){
-	if(h==0 || h>H ||w==0 ||w>W){
-		return;
-	}
-	if(G[h][w]==1){
-		G[h][w]=0;
-		f(h-1,w);
-		f(h+1,w);
-		f(h,w-1);
-		f(h,w+1);
-		f(h+1,w+1);
-		f(h+1,w-1);
-		f(h-1,w+1);
-		f(h-1,w-1);
-	}
+void f(int h, int w) {
+  if (h == 0 || h > H || w == 0 || w > W) {
+    return;
+  }
+  if (G[h][w] == 1) {
+    G[h][w] = 0;
+    f(h - 1, w);
+    f(h + 1, w);
+    f(h, w - 1);
+    f(h, w + 1);
+    f(h + 1, w + 1);
+    f(h + 1, w - 1);
+    f(h - 1, w + 1);
+    f(h - 1, w - 1);
+  }
 }
 
 int main() {
   int tmp;
-	int tresult;
+  int tresult;
   while (true) {
     cin >> W >> H;
     if (H == 0 && W == 0) {
       break;
     }
-		tresult=0;
+    tresult = 0;
     G.clear();
     G.resize(W + 1, vector<int>(H + 1, 0));
     for (int h = 1; h <= H; h++) {
@@ -40,24 +40,24 @@ int main() {
         G[h][w] = tmp;
       }
     }
-		for (int h=1;h<=H;h++){
-			for(int w=1;w<=W;w++){
-				if(G[h][w]==1){
-					tresult++;
-					f(h,w);
-				}
-			}
-		}
-		result.push_back(tresult);
-//    for (int h = 1; h <= H; h++) {
-//      for (int w = 1; w <= W; w++) {
-//      	cout<<G[h][w]<<" ";
-//			}
-//			cout<<endl;
-//    }
+    for (int h = 1; h <= H; h++) {
+      for (int w = 1; w <= W; w++) {
+        if (G[h][w] == 1) {
+          tresult++;
+          f(h, w);
+        }
+      }
+    }
+    result.push_back(tresult);
+    //    for (int h = 1; h <= H; h++) {
+    //      for (int w = 1; w <= W; w++) {
+    //      	cout<<G[h][w]<<" ";
+    //			}
+    //			cout<<endl;
+    //    }
   }
-		for(int atom:result){
-			cout<<atom<<endl;
-		}
+  for (int atom : result) {
+    cout << atom << endl;
+  }
   return 0;
 }
