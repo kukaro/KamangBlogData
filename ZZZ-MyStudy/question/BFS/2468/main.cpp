ffff5a07324c46ab4b3a0cf7188fd1c6b91032c8
kukaro <justkukaro@naver.com>
2018-06-05 02:24:30 +0000

    mark

--- ZZZ-MyStudy/question/BFS/2468/main.cpp
+++ ZZZ-MyStudy/question/BFS/2468/main.cpp
@@ -55,14 +55,14 @@ int bfs(int n) {
       }
     }
   }
-//  cout << "**********" << endl;
-//  for (int i = 1; i <= N; i++) {
-//    for (int j = 1; j <= N; j++) {
-//      cout << ans[i][j] << " ";
-//    }
-//    cout << endl;
-//  }
-//  cout << "**********" << endl;
+  cout << "**********" << endl;
+  for (int i = 1; i <= N; i++) {
+    for (int j = 1; j <= N; j++) {
+      cout << ans[i][j] << " ";
+    }
+    cout << endl;
+  }
+  cout << "**********" << endl;
   return result_val;
 }
 
@@ -81,6 +81,7 @@ int main() {
     ans.clear();
     ans.resize(N + 1, vector<int>(N + 1, 0));
     tmp = bfs(n);
+//		cout<<tmp<<"*"<<endl;
     max_val = max_val > tmp ? max_val : tmp;
   }
   cout << max_val << endl;
