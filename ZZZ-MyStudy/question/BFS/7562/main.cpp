4b64906ec20426d9578dd62a16a57e20bab06e3d
kukaro <justkukaro@naver.com>
2018-06-08 13:43:29 +0000

    7562

---
+++ ZZZ-MyStudy/question/BFS/7562/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Point {
+public:
+  int x;
+  int y;
+  Point(){};
+  Point(int x, int y) : x(x), y(y){};
+};
+
+int T;
+int N;
+queue<Point> q;
+Point S, L;
+vector<vector<int>> ans;
+vector<int> res;
+
+int bfs(){
+	Point tmp;
+	q.push(S);
+	while(!q.empty()){
+		tmp = q.front();
+		q.pop();
+		//cout<<tmp.x<<":"<<tmp.y<<endl;
+		ans[tmp.x][tmp.y]++;
+		if(tmp.x==L.x && tmp.y==L.y){
+			break;
+		}
+		if(tmp.x+1<=N && tmp.y-2>0 && ans[tmp.x+1][tmp.y-2]==0){
+			q.push(Point(tmp.x+1,tmp.y-2));
+		}
+		if(tmp.x+2<=N && tmp.y-1>0 && ans[tmp.x+2][tmp.y-1]==0){
+			q.push(Point(tmp.x+2,tmp.y-1));
+		}
+		if(tmp.x+2<=N && tmp.y+1<=N && ans[tmp.x+2][tmp.y+1]==0){
+			q.push(Point(tmp.x+1,tmp.y+1));
+		}
+		if(tmp.x+1<=N && tmp.y+2<=N && ans[tmp.x+1][tmp.y+2]==0){
+			q.push(Point(tmp.x+1,tmp.y+2));
+		}
+		if(tmp.x-1>0 && tmp.y+2<=N && ans[tmp.x-1][tmp.y+2]==0){
+			q.push(Point(tmp.x-1,tmp.y+2));
+		}
+		if(tmp.x-2>0 && tmp.y+1<=N && ans[tmp.x-2][tmp.y+1]==0){
+			q.push(Point(tmp.x-2,tmp.y+1));
+		}
+		if(tmp.x-2>0 && tmp.y-1>0 && ans[tmp.x-2][tmp.y-1]==0){
+			q.push(Point(tmp.x-2,tmp.y-1));
+		}
+		if(tmp.x-1>0 && tmp.y-2>0 && ans[tmp.x-1][tmp.y-2]==0){
+			q.push(Point(tmp.x-1,tmp.y-2));
+		}
+	}
+	return ans[L.x][L.y];
+}
+
+int main() {
+  cin >> T;
+  for (int t = 0; t < T; t++) {
+    cin >> N;
+    cin >> S.x >> S.y >> L.x >> L.y;
+		ans.clear();
+		ans.resize(N+1,vector<int>(N+1,0));
+		res.push_back(bfs());
+  }
+	for(int atom:res){
+		cout<<atom<<endl;
+	}
+}
