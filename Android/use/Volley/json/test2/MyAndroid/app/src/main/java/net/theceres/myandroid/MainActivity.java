745084f745b30786f23450dbdb1aaed2757ce15c
kukaro <justkukaro@naver.com>
2018-06-11 13:21:55 +0900

    and

--- Android/use/Volley/json/test2/MyAndroid/app/src/main/java/net/theceres/myandroid/MainActivity.java
+++ Android/use/Volley/json/test2/MyAndroid/app/src/main/java/net/theceres/myandroid/MainActivity.java
@@ -1,62 +1,35 @@
-package net.theceres.myapplication;
+package net.theceres.myandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
     private static final String TAG = "MAIN";
     private TextView tv;
     private RequestQueue queue;
-
-    private String id;
-    private String pw;
-    private JSONArray language;
-    private JSONObject item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.tvMain);
-
+        tv = findViewById(R.id.textMain);
         queue = Volley.newRequestQueue(this);
-        String url = "http://192.168.0.9:5000/";
+        String url = "http://www.google.com";
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                JsonParser jsonParser = new JsonParser();
-                JsonElement jsonElement = jsonParser.parse(response);
-                tv.setText(jsonElement + "\n");
-                tv.setText(tv.getText() + jsonElement.getClass().getName() + "\n");
-                tv.setText(tv.getText() + ((JsonObject) jsonElement).get("language").getClass().getName() + "\n");
+                Log.e(TAG, "onResponse: hi");
+                tv.setText(response);
             }
         }, new Response.ErrorListener() {
             @Override
@@ -72,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        if (queue != null) {
+        if(queue!=null){
             queue.cancelAll(TAG);
         }
     }
