f8e3c4088cba17a3dd6872ba79479e370b6fcd83
kukaro <justkukaro@naver.com>
2018-06-11 23:30:18 +0000

    asdf

---
+++ ZZZ-MyStudy/question/Simulation/1094/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int X;
+int cnt;
+int res=0;
+int dest=64;
+
+int main(){
+	cin>>X;
+	cnt = 0;
+	while(res!=X){
+		if(X==64){
+			cnt++;
+			break;
+		}
+		dest = dest/2;
+		if(res+dest<=X){
+			res += dest;
+			cnt++;
+		}
+	}
+	cout<<cnt<<endl;
+	return 0;
+}
