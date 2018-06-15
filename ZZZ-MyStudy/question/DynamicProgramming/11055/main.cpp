4c7356eb8a52657fe4c1c6946f17edb8518e9690
kukaro <justkukaro@naver.com>
2018-06-15 05:12:07 +0000

    refact

--- ZZZ-MyStudy/question/DynamicProgramming/11055/main.cpp
+++ ZZZ-MyStudy/question/DynamicProgramming/11055/main.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int N;
@@ -7,35 +7,35 @@ vector<int> A;
 vector<int> memo;
 int max_val;
 
-void print_memo(){
-	for(int n=1;n<=N;n++){
-		cout<<memo[n]<<" ";
-	}
-	cout<<endl;
+void print_memo() {
+  for (int n = 1; n <= N; n++) {
+    cout << memo[n] << " ";
+  }
+  cout << endl;
 }
 
-int main(){
-	int tmp;
-	int tm;
-	cin>>N;
-	A.push_back(0);
-	memo.resize(N+1,0);
-	for(int n=1;n<=N;n++){
-		cin>>tmp;
-		A.push_back(tmp);
-	}
-	for(int n=1;n<=N;n++){
-		memo[n] = tmp = A[n];
-		tm = 0;
-		for(int i=n-1;i>0;i--){
-			if(A[i]<tmp && tm<memo[i]){
-				tm = memo[i];
-			}
-		}
-		memo[n]+=tm;
-		max_val = max_val>memo[n]?max_val:memo[n];
-	}
-	//print_memo();
-	cout<<max_val<<endl;
-	return 0;
+int main() {
+  int tmp;
+  int tm;
+  cin >> N;
+  A.push_back(0);
+  memo.resize(N + 1, 0);
+  for (int n = 1; n <= N; n++) {
+    cin >> tmp;
+    A.push_back(tmp);
+  }
+  for (int n = 1; n <= N; n++) {
+    memo[n] = tmp = A[n];
+    tm = 0;
+    for (int i = n - 1; i > 0; i--) {
+      if (A[i] < tmp && tm < memo[i]) {
+        tm = memo[i];
+      }
+    }
+    memo[n] += tm;
+    max_val = max_val > memo[n] ? max_val : memo[n];
+  }
+  // print_memo();
+  cout << max_val << endl;
+  return 0;
 }
