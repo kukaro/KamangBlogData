87993d237ab9e1f538d1391f707445be3d74d296
kukaro <justkukaro@naver.com>
2018-06-20 06:41:27 +0900

    6.4

---
+++ ZZZ-MyStudy/멀티미디어응용-김종남/20180620/6_4_2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <opencv/cv.hpp>
+
+using namespace std;
+using namespace cv;
+
+string str = "/Users/kukaro/KamangBlogData/ZZZ-MyStudy/멀티미디어응용-김종남/20180620/";
+
+int main(int argc, const char *argv[]) {
+    Mat BGR_img = imread(str + "color_model.jpg", IMREAD_COLOR);
+    CV_Assert(BGR_img.data);
+
+    Scalar white(255, 255, 255);
+    Mat CMY_img = white - BGR_img;
+    Mat CMY_arr[3];
+    split(CMY_img, CMY_arr);
+
+    Mat black;
+    min(CMY_arr[0], CMY_arr[1], black);
+    min(black, CMY_arr[2], black);
+
+    CMY_arr[0] = CMY_arr[0] - black;
+    CMY_arr[1] = CMY_arr[1] - black;
+    CMY_arr[2] = CMY_arr[2] - black;
+
+    imshow("Black", black);
+    imshow("Yellow", CMY_arr[0]);
+    imshow("Magenta", CMY_arr[1]);
+    imshow("Cyan", CMY_arr[2]);
+    waitKey();
+    return 0;
+}
