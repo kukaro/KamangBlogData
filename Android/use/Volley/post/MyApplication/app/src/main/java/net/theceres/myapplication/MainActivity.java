9a8e39b914425fe4540fd5438c5febfb0a70b0c6
kukaro <justkukaro@naver.com>
2018-06-22 22:38:22 +0900

    postgre

--- Android/use/Volley/post/MyApplication/app/src/main/java/net/theceres/myapplication/MainActivity.java
+++ Android/use/Volley/post/MyApplication/app/src/main/java/net/theceres/myapplication/MainActivity.java
@@ -1,5 +1,8 @@
 package net.theceres.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -43,37 +46,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.tvMain);
-
-        queue = Volley.newRequestQueue(this);
-        String url = "http://192.168.0.9:5000/";
-
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                JsonParser jsonParser = new JsonParser();
-                JsonElement jsonElement = jsonParser.parse(response);
-                tv.setText(jsonElement + "\n");
-                tv.setText(tv.getText() + jsonElement.getClass().getName() + "\n");
-                tv.setText(tv.getText() + ((JsonObject) jsonElement).get("language").getClass().getName() + "\n");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO) && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_AUDIO_AND_WRITE_EXTERNAL_STORAGE);
+            } else {
+                // 유저가 거부하면서 다시 묻지 않기를 클릭.. 권한이 없다고 유저에게 직접 알림.
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-
-        stringRequest.setTag(TAG);
-        queue.add(stringRequest);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (queue != null) {
-            queue.cancelAll(TAG);
+        } else {
+            startUsingSpeechSDK();
         }
     }
 }
