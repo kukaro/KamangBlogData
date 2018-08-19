d818723371fdac0064e613a52c7dc77cfb322776
kukaro <justkukaro@naver.com>
2018-08-19 15:19:42 +0900

    learn

---
+++ ZZZ-MyStudy/deeplearning/cpp/5_1/Variable.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 김태정 on 2018. 8. 16..
+//
+
+#include "Variable.h"
---
+++ ZZZ-MyStudy/deeplearning/cpp/5_1/Variable.h
@@ -0,0 +1,15 @@
+//
+// Created by 김태정 on 2018. 8. 16..
+//
+
+#ifndef INC_5_1_VARIABLE_H
+#define INC_5_1_VARIABLE_H
+
+
+class Variable {
+private:
+    
+};
+
+
+#endif //INC_5_1_VARIABLE_H
