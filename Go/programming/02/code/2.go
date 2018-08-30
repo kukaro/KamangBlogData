662b170a9b1532c517271603476dee8263852259
kukaro <justkukaro@naver.com>
2018-08-31 05:40:26 +0900

    commit

---
+++ Go/programming/02/code/2.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int
+	a = 10
+
+	var b = 20
+
+	c := 30
+
+	fmt.Println(a,b,c)
+}
+
---
+++ Go/programming/02/code/3.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+"fmt"
+)
+
+func main() {
+	var a, b, c int
+	var (
+		d string
+		e rune
+		f float32
+	)
+	a = 10
+	b = 20
+	c = 30
+	d = "hi"
+	e = 'h'
+	f = 3.14
+	fmt.Println(a, b, c, d, e, f)
+}
