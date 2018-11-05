f3a6bff64a692c8ef69c091aa744a92ff7e12593
kukaro <justkukaro@naver.com>
2018-11-06 02:58:13 +0900

    vie

---
+++ Vue/Ch14/my-vue/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<!--HelloWorld.vue-->
+<template>
+  <div class="hello">
+    <b>count : {{this.$store.state.count}}</b><br>
+    <input type="button" @click="increment()" value="increment"/>
+    <input type="button" @click="decrement()" value="decrement"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+
+    }
+  },
+  created: function () {
+    console.log(this.$store)
+  },
+  methods: {
+    increment: function () {
+      this.$store.commit('increment')
+    },
+    decrement: function () {
+      this.$store.commit('decrement')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
