72f9c7c579d3615527d2b176a88a5f1ad9bd29c1
kukaro <justkukaro@naver.com>
2019-01-14 15:17:10 +0900

    react

---
+++ React/react-study/104.js
@@ -0,0 +1,8 @@
+var items = ['1', '2', '3', '4'];
+
+function printStuff(a, b, c) {
+    console.log(`Printing: ${a} ${b} ${c}`);
+}
+
+printStuff(items[0], items[1], items[2]);
+printStuff(...items);
