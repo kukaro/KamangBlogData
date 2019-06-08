2aa21a32177b977511ba7a8968c81eaf04d8011a
kukaro <justkukaro@naver.com>
2019-06-08 17:28:19 +0000

    graphql

---
+++ JavaSciprt/babel/Ch01/babeltest/src/Person.js
@@ -0,0 +1,15 @@
+// Person.js
+class Person {
+
+    constructor(){
+        this.name = 'kukaro';
+    }
+
+    getName() {
+        return this.name;
+    }
+}
+
+console.log(10 ** 2);
+
+export default Person;
