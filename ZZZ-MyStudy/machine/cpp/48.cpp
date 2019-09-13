63ced129689ea591e4a81c280e33510370fdd762
kukaro <justkukaro@naver.com>
2019-09-13 16:42:55 +0000

    48p

---
+++ ZZZ-MyStudy/machine/cpp/48.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+float dot(float *v1, float *v2, int len) {
+    float sum = 0;
+    for (int i = 0; i < len; i++) {
+        sum += v1[i] * v2[i];
+    }
+    return sum;
+}
+
+float step(float v) {
+    return v > 0 ? 1 : 0;
+}
+
+float forward(float *x, float *w, int len) {
+    float u = dot(x, w, len);
+    return step(u);
+}
+
+void train(float *w, float *x, float t, float e, int len) {
+    float z = forward(x, w, len);
+    for (int j = 0; j < len; j++) {
+        w[j] += (t - z) * x[j] * e;
+    }
+}
+
+#define DATA_NUMS 4
+#define WEIGHT_NUMS 3
+
+int main() {
+    float e = 0.1;
+    float x[DATA_NUMS][WEIGHT_NUMS] = {
+            {1, 0, 0},
+            {1, 0, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+    };
+    float t[DATA_NUMS] = {0, 0, 0, 1};
+//    float t[DATA_NUMS] = {0, 1, 1, 1};
+    float w[WEIGHT_NUMS] = {0, 0, 0};
+
+    int epoch = 10;
+    for (int i = 0; i < epoch; i++) {
+        cout << "epoch:" << i << " ";
+        for (int j = 0; j < DATA_NUMS; j++) {
+            train(w, x[j], t[j], e, WEIGHT_NUMS);
+        }
+        for (int j = 0; j < WEIGHT_NUMS; j++) {
+            cout << "w" << j << ":" << w[j] << " ";
+        }
+        cout << endl;
+    };
+    for (int i = 0; i < DATA_NUMS; i++) {
+        cout << forward(x[i], w, WEIGHT_NUMS) << " ";
+    }
+    cout << endl;
+
+    for (float atom:w) {
+        cout << atom << " ";
+    }
+    cout << endl;
+    return 0;
+}
